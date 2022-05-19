b682d826229851add1ab824f2fe767a783a7971d
Ricardo <sgroja6@gmail.com>
2022-05-18 23:52:24 -0500

    update webflux

--- createProduct/src/main/java/com/nttdata/createProduct/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nttdata.createProduct.repository;
-
-import com.nttdata.createProduct.entity.Client;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClientRepository  extends MongoRepository <Client, String> {
-    
-}
